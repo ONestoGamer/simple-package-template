7f43debf6eeea52d91b727138c33e45e6a5d2e54
Ernesto da Silva Santana <ernestoo.es@gmail.com>
2025-11-23 01:13:41 -0300

    Update package metadata in setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     page_description = f.read()
 
-with open("requirements.txt") as f:
+with open("requirements.txt", "r") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image_processing_package",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Ernesto Silva",
+    author_email="ernestoo.es@gmail.com",
+    description="A package for basic image processing tasks",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/ONestoGamer/simple-package-template",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.6',
 )
